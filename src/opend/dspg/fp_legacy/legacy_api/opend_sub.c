0681228f4fa1b10401297a053e619a60b0eb109e
Patrick Weber <patrick.weber@stackforce.de>
2019-03-04 13:50:15 +0100

    Deletes old CMBS legacy callback functions at DSPG FP.

--- src/opend/dspg/fp_legacy/legacy_api/opend_sub.c
+++ src/opend/dspg/fp_legacy/legacy_api/opend_sub.c
@@ -54,9 +54,6 @@ openD_status_t openD_subApi_init( openD_subApiPrimitives_t *sPrimitives )
     _sPrimitives.openD_subApiCfm = sPrimitives->openD_subApiCfm;
     _sPrimitives.openD_subApiInd = sPrimitives->openD_subApiInd;
 
-    initMsgParserSub(registerSuccessClb);
-    initMsgParserDeSub(opend_deregistration_finished_cllb);
-
     return OPEND_STATUS_OK;
 }
 
